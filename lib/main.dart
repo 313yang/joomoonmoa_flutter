ce8a206720f36608b9d8c2dae263686c23685160
Yang⭐ <78190786+313yang@users.noreply.github.com>
2024-05-18 19:07:42 +0900

    feat: firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyWebView());
 }
 
