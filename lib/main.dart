6034ac52cde986f5997fbd1980a08d56823d6885
Yang⭐ <78190786+313yang@users.noreply.github.com>
2024-05-23 21:49:36 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,44 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+// Future _dailyAtTimeNotification() async {
+//   const notiTitle = 'title';
+//   const notiDesc = 'description';
+
+//   final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+//   final result = await flutterLocalNotificationsPlugin
+//       .resolvePlatformSpecificImplementation<
+//           IOSFlutterLocalNotificationsPlugin>()
+//       ?.requestPermissions(
+//         alert: true,
+//         badge: true,
+//         sound: true,
+//       );
+
+//   var android = const AndroidNotificationDetails('id', notiTitle, notiDesc,
+//       importance: Importance.max, priority: Priority.max);
+//   // var ios = IOSNotificationDetails();
+//   var detail = NotificationDetails(android: android);
+
+//   if (result) {
+//     await flutterLocalNotificationsPlugin
+//         .resolvePlatformSpecificImplementation<
+//             AndroidFlutterLocalNotificationsPlugin>()
+//         ?.deleteNotificationChannelGroup('id');
+
+//     await flutterLocalNotificationsPlugin.zonedSchedule(
+//       0, // id는 unique해야합니다. int값
+//       notiTitle,
+//       notiDesc,
+//       detail,
+//       androidAllowWhileIdle: true,
+//       uiLocalNotificationDateInterpretation:
+//           UILocalNotificationDateInterpretation.absoluteTime,
+//       matchDateTimeComponents: DateTimeComponents.time,
+//     );
+//   }
+// }
+
 const AndroidNotificationChannel channel = AndroidNotificationChannel(
   'high_importance_channel', // id
   'High Importance Notifications', // title
@@ -52,9 +90,22 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
         notification?.title,
         notification?.body,
         NotificationDetails(
-          android: AndroidNotificationDetails(channel.id, channel.name,
-              importance: Importance.high, icon: "ic_launcher.png"),
+          android: AndroidNotificationDetails(
+            channel.id,
+            channel.name,
+            importance: Importance.high,
+            icon: "@mipmap/ic_launcher",
+          ),
         ),
+      );
+      await FirebaseMessaging.instance.requestPermission(
+        alert: true,
+        announcement: true,
+        badge: true,
+        carPlay: true,
+        criticalAlert: true,
+        provisional: true,
+        sound: true,
       );
       print('Background;:Message received: ${message.notification?.title}');
       print('Background;:Message received: ${message.notification?.body}');
@@ -66,7 +117,17 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // await FirebaseMessaging.instance.requestPermission(
+  //   alert: true,
+  //   announcement: true,
+  //   badge: true,
+  //   carPlay: true,
+  //   criticalAlert: true,
+  //   provisional: true,
+  //   sound: true,
+  // );
 
+  _initNotiSetting();
   // await Firebase.initializeApp(
   //   options: DefaultFirebaseOptions.currentPlatform,
   // );
@@ -87,6 +148,24 @@ void main() async {
   runApp(const MyWebView());
 }
 
+void _initNotiSetting() async {
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  const initSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  // final initSettingsIOS = IOSInitializationSettings(
+  //   requestSoundPermission: false,
+  //   requestBadgePermission: false,
+  //   requestAlertPermission: false,
+  // );
+  const initSettings = InitializationSettings(
+    android: initSettingsAndroid,
+    // iOS: initSettingsIOS,
+  );
+  await flutterLocalNotificationsPlugin.initialize(
+    initSettings,
+  );
+}
+
 class MyWebView extends StatefulWidget {
   const MyWebView({super.key});
 
@@ -104,6 +183,9 @@ class _MyWebViewState extends State<MyWebView> {
       ..loadRequest(Uri.parse('https://www.joomoonmoa.com')) //출력할 웹페이지
       ..setJavaScriptMode(JavaScriptMode.unrestricted);
     super.initState();
+
+    var initializationSettingsAndroid =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
     // initAndroid();
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       // Handle the message when the app is in the foreground
