3f1aac2267a11512074bde421ed3f65ef90102ed
Yang⭐ <78190786+313yang@users.noreply.github.com>
2024-05-23 21:22:17 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,65 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  description: 'This channel is used for important notifications.',
+  importance: Importance.high,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void initAndroid() async {
+  // Android용 새 Notification Channel
+  // const AndroidNotificationChannel androidNotificationChannel =
+  //     AndroidNotificationChannel(
+  //   'high_importance_channel', // 임의의 id
+  //   'High Importance Notifications', // 설정에 보일 채널명
+  //   importance: Importance.max,
+  // );
+
+  // Notification Channel을 디바이스에 생성
+  // final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  //     FlutterLocalNotificationsPlugin();
+  // await flutterLocalNotificationsPlugin
+  //     .resolvePlatformSpecificImplementation<
+  //         AndroidFlutterLocalNotificationsPlugin>()
+  //     ?.createNotificationChannel(androidNotificationChannel);
+}
+
+/// E/flutter (24168): [ERROR:flutter/runtime/dart_vm_initializer.cc(41)] Unhandled Exception: PlatformException(error, Attempt to invoke virtual method 'int java.lang.Integer.intValue()' on a null object reference, null, java.lang.NullPointerException: Attempt to invoke virtual method 'int java.lang.Integer.intValue()' on a null object reference
 void getToken() async {
   final fcmToken = await FirebaseMessaging.instance.getToken();
   print("token:::$fcmToken");
 }
 
+@pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
   await Firebase.initializeApp();
-
-  print('Background;:Message received: ${message.notification?.title}');
-  print('Background;:Message received: ${message.notification?.body}');
+  final notification = message.notification;
+  if (message.notification != null) {
+    try {
+      flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        notification?.title,
+        notification?.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(channel.id, channel.name,
+              importance: Importance.high, icon: "ic_launcher.png"),
+        ),
+      );
+      print('Background;:Message received: ${message.notification?.title}');
+      print('Background;:Message received: ${message.notification?.body}');
+    } catch (e) {
+      print(e);
+    }
+  }
 }
 
 void main() async {
@@ -58,6 +104,7 @@ class _MyWebViewState extends State<MyWebView> {
       ..loadRequest(Uri.parse('https://www.joomoonmoa.com')) //출력할 웹페이지
       ..setJavaScriptMode(JavaScriptMode.unrestricted);
     super.initState();
+    // initAndroid();
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       // Handle the message when the app is in the foreground
       print('Message received: ${message.notification?.title}');
